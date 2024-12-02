d5b35141dac0a83330a860bb734fd36625281ae3
heehong <gmlghd98@gmail.com>
2024-12-02 13:26:00 +0900

    💄 물건 출력

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ function App() {
   return (
     <div>
       <Header />
+      <ProductList />
+      <Container />
     </div>
   );
 }
 
 function Header() {
   return <h1>React 프로그래밍</h1>;
+}
+
+function ProductList() {
+  return <div className="productList">KIT 재고 현황</div>;
+}
+
+function Container() {
+  return (
+    <div className="container">
+      <img src="https://picsum.photos/600/150?random=1" alt="랜덤 이미지 1" />
+      <div className="product">물건</div>
+    </div>
+  );
 }
 
 export default App;
