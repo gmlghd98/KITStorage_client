becc2f8080615540b784ce8bb67dd3047cac1fda
heehong <gmlghd98@gmail.com>
2024-12-02 13:41:18 +0900

    🍇 물건 제목 배열 사용

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import Container from './Container';
 
 const App = () => {
+  const products = ['물건1', '물건2', '물건3'];
+
   return (
     <div>
       <Header />
-      <ProductList />
-      <Container title="물건 1" index="1" />
-      <Container title="물건 2" index="2" />
-      <Container title="물건 3" index="3" />
+      <ProductList title="KIT 재고 현황" listProduct={products} />
     </div>
   );
 };
@@ -17,8 +16,15 @@ const Header = () => {
   return <h1>React 프로그래밍</h1>;
 };
 
-const ProductList = () => {
-  return <div className="productList">KIT 재고 현황</div>;
+const ProductList = (props) => {
+  return (
+    <div className="productList">
+      <div className="productList">{props.title}</div>
+      {props.listProduct.map((product, index) => (
+        <Container key={index} title={product} index={index} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
